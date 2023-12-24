eed38de9c671c8f4f3b4ff9449a6726fe3dc1c69
KarthickSakthi <karthicksakthi124@gmail.com>
2023-12-24 22:24:51 +0530

    hardhat and contract changes

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -6,37 +6,40 @@ import "@chainlink/contracts/src/v0.8/vrf/VRFConsumerBaseV2.sol";
 
 error Raffle_NotEnoughETHEntered();
 
-contract Raffle{
-    // State variables
-    uint256 private immutable i_entranceFee;
-    address payable[] private s_players;
+contract Raffle is VRFConsumerBaseV2 {
+  // State variables
+  uint256 private immutable i_entranceFee;
+  address payable[] private s_players;
 
-    // Events
-    event RaffleEnter(address indexed player);
+  // Events
+  event RaffleEnter(address indexed player);
 
-    constructor(address vrfCoordinatorV2, uint256 entranceFee) VRFConsumerBaseV2(vrfCoordinatorV2){
-        i_entranceFee = entranceFee;
-    }
+  constructor(
+    address vrfCoordinatorV2,
+    uint256 entranceFee
+  ) VRFConsumerBaseV2(vrfCoordinatorV2) {
+    i_entranceFee = entranceFee;
+  }
 
-    function enterRaffle() public payable{
-        // require(msg.value > i_entranceFee, "Not enough ETH!")
-        if(msg.value < i_entranceFee)
-          revert Raffle_NotEnoughETHEntered();
-        s_players.push(payable(msg.sender))
-        emit RaffleEnter(msg.sender);
-    }
+  function enterRaffle() public payable {
+    // require(msg.value > i_entranceFee, "Not enough ETH!")
+    if (msg.value < i_entranceFee) revert Raffle_NotEnoughETHEntered();
+    s_players.push(payable(msg.sender));
+    emit RaffleEnter(msg.sender);
+  }
 
-    function requestRandomWinner() external{}
+  function requestRandomWinner() external {}
 
-    function fullfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override{
+  function fullfillRandomWords(
+    uint256 requestId,
+    uint256[] memory randomWords
+  ) internal override {}
 
-    }
+  function getEntranceFee() public view returns (uint256) {
+    return i_entranceFee;
+  }
 
-    function getEntranceFee() public view  returns (uint256) {
-        return i_entranceFee;
-    }
-
-    function getPlayer(uint256 index) public view returns(address){
-        return s_players[index];
-    }
+  function getPlayer(uint256 index) public view returns (address) {
+    return s_players[index];
+  }
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@hardhat-shorthand");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
