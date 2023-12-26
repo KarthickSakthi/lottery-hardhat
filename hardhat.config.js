19503521b87e8b457a93941369141d5e042c4cfb
KarthickSakthi <karthicksakthi124@gmail.com>
2023-12-26 14:05:06 +0530

    Updated Raffle lottery contract and config changes

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
-require("@hardhat-shorthand");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
+  solidity: "0.8.7",
 };
